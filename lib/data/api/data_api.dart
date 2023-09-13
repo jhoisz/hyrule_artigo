3c5c3f73684da782c42027e8fbb9486f3851b3b5
jhoisz <jvrda@ufpi.edu.br>
2023-09-13 14:48:11 -0300

    Usando url de Utils

--- lib/data/api/data_api.dart
+++ lib/data/api/data_api.dart
@@ -1,12 +1,13 @@
 import 'package:dio/dio.dart';
 
 import '../../domain/models/entry.dart';
+import '../../utils/consts/api.dart';
 
 class DataApi {
   final Dio dio = Dio();
 
   Future<List<Entry>> getEntriesByCategory({required String category}) async {
-    final response = await dio.get('https://botw-compendium.herokuapp.com/api/v3/compendium/category/$category');
+    final response = await dio.get(url + category);
 
     return List<Entry>.from(response.data["data"].map((e) => Entry.fromMap(e)));
   } 
